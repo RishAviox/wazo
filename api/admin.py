44466b80cf81b94330181247ead3c0e5d0b593a3
Mudasar-Makandar <mudassarmmakandar@gmail.com>
2024-04-24 09:52:17 +0530

    add logs model and middleware

--- api/admin.py
+++ api/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
-from .models import WajoUser
+from .models import WajoUser, APILog
 
 # Register your models here.
 
 class WajoUserAdmin(admin.ModelAdmin):
     list_display = ('phone_no', 'selected_language', 'fcm_token', 'created_on', 'updated_on', )
 
+class APILogAdmin(admin.ModelAdmin):
+    list_display = ['user', 'method', 'path', 'status_code', 'created_at']
+    list_filter = ['method', 'status_code', 'created_at']
+    search_fields = ['path']
+
+
 admin.site.register(WajoUser, WajoUserAdmin)
+admin.site.register(APILog, APILogAdmin)
--- api/models.py
+++ api/models.py
@@ -43,3 +43,16 @@ class WajoUser(models.Model):
             return False
     
 
+
+class APILog(models.Model):
+    user = models.ForeignKey(WajoUser, on_delete=models.SET_NULL, null=True, blank=True)
+    method = models.CharField(max_length=10)
+    path = models.CharField(max_length=255)
+    status_code = models.IntegerField()
+    request_body = models.TextField(blank=True, null=True)
+    response_message = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.method} {self.path} {self.status_code} {self.created_at}"
+
