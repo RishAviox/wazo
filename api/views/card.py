ff5cee6081e9780c793b3045d8e13f55fa8acd60
Mudasar-Makandar <mudassarmmakandar@gmail.com>
2024-06-19 11:29:46 +0530

    get status card metrics for all the players of the coach

--- api/views/card.py
+++ api/views/card.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 
-from api.serializer import CardSuggestedActionsSerializer, StatusCardMetricsSerializer
+from api.serializer import CardSuggestedActionsSerializer, StatusCardMetricsSerializer, WajoUserSerializer
 from api.models import CardSuggestedAction, StatusCardMetrics
 from api.utils import *
 
@@ -14,12 +14,33 @@ class StatusCardMetricAPI(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
-        try:
-            actions = StatusCardMetrics.objects.filter(user=request.user).latest('updated_on')
-            serializer = StatusCardMetricsSerializer(actions)
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        except:
-            return Response({ 'error': 'status metrics data not found'}, status=status.HTTP_400_BAD_REQUEST)
+
+        if request.user.role == 'coach':
+            player_data = []
+            for player in request.user.players.all():
+                try:
+                    metrics = StatusCardMetrics.objects.filter(user=player).latest('updated_on')
+                    player_data.append({
+                                'profile': WajoUserSerializer(player).data,
+                                'metrics': StatusCardMetricsSerializer(metrics).data
+                            })
+
+                except StatusCardMetrics.DoesNotExist:
+                    player_data.append({
+                                'profile': WajoUserSerializer(player).data,
+                                'metrics': {}
+                            })
+
+            print("player_data for coach: ", player_data)
+            return Response(player_data, status=status.HTTP_200_OK)
+        
+        else:
+            try:
+                metrics = StatusCardMetrics.objects.filter(user=request.user).latest('updated_on')
+                serializer = StatusCardMetricsSerializer(metrics)
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            except:
+                return Response({ 'error': 'status metrics data not found'}, status=status.HTTP_400_BAD_REQUEST)
 
 
 
