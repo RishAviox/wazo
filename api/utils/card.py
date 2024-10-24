8b9fe941ef9d9e68ca3b862a358be987dfda7823
Mudasar-Makandar <mudassarmmakandar@gmail.com>
2024-10-24 17:57:52 +0530

    add video card ai insight

--- api/utils/card.py
+++ api/utils/card.py
@@ -169,7 +169,7 @@ def calculate_recurrence_dates(event, start_date, end_date):
 
 def get_daily_snapshot(user, event_date):
     one_time_events, recurring_events = get_events_for_date(user=user, event_date=event_date)
-
+    print("one_time_events: ", list(one_time_events), recurring_events)
     one_time_events_data = [
         {
             'event_type': event.event_type,
@@ -1388,4 +1388,42 @@ def get_prompt_for_insight(user, card):
             prompt = f"Generate a concise expert analysis for athletes only in {language} language based on the provided GPS football performance metrics, highlighting key strengths, areas for improvement, and suggesting targeted drills. Keep it under 40 words and avoid mentioning the data passed to you or athletes and coaches. Make sure the sentence can be directly sent to the player. Data provided:{user_data}. Example: 'You have strong high-speed runs and cover good distance, but work on improving your recovery speed and positioning in transitions. Focus on drills for endurance, agility, and quick direction changes.' {'Dont translate but think and respond in Hebrew.' if language == 'Hebrew' else ''}"
             return prompt
     
+    elif card == 'VideoCard':
+        today = datetime.today()
+        if user.role == 'Coach':
+            player_data = []
+            for player in user.players.all():
+                user_data = {
+                        "wellness": get_status_card_metrics(player),
+                        "calendar": get_daily_snapshot(player, today),
+                        "defensive-skills-metrics":get_videocard_defensive_metrics(player),
+                        "gps-football-abilities-metrics": get_gps_football_abilities_metrics(player),
+                        'gps-athletic-skills-metrics': get_gps_athletic_skills_metrics(player)
+                    }
+                player_data.append(user_data)
+
+            user_data = {
+                'team-VideoCard': player_data
+            }
+            print("team-gps-football-abilities-metrics for coach: ", user_data)
+            prompt = f"Generate a concise expert analysis for coaches only in {language} language based on the provided calendar data, team wellness data, and offensive and defensive performance metrics. This is mandatory. Analyze the team’s last game and provide one key focus area for reviewing game footage. Also, consider wellness and other stats to set the priority for the team. Avoid mentioning the data passed to you or athletes and coaches. Data provided: {user_data}. Please make sure to address the coach directly. Maximum 40 words.' {'Dont translate but think and respond in Hebrew.' if language == 'Hebrew' else ''}"
+            return prompt
+        
+        else:
+            user_data = {
+                        "wellness": get_status_card_metrics(user),
+                        "calendar": get_daily_snapshot(user, today),
+                        "defensive-skills-metrics":get_videocard_defensive_metrics(user),
+                        "gps-football-abilities-metrics": get_gps_football_abilities_metrics(user),
+                        'gps-athletic-skills-metrics': get_gps_athletic_skills_metrics(user)
+                    }
+            user_data = {
+                'player-VideoCard': user_data
+            }
+            prompt = f"Generate a concise expert analysis for athletes only in {language} language based on the provided calendar data, their wellness data, and offensive and defensive performance metrics. This is mandatory. Analyze their last game and provide one key focus area while watching their game footage. Also, consider wellness and other stats to set the priority for the user. Avoid mentioning the data passed to you or athletes and coaches. Data provided: {user_data}. Please make sure to address the athlete in second person and not in third person. Maximum 40 words {'Dont translate but think and respond in Hebrew.' if language == 'Hebrew' else ''}"
+            return prompt
+
+    # elif card == 'TrainingCard':
+    #     pass
+
     return None
