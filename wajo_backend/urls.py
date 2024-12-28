7cd429e19a4df2676578b59884c9b489cae95259
Mudasar-Makandar <mudassarmmakandar@gmail.com>
2024-12-28 11:16:16 +0530

    remove v1 version from urls

--- wajo_backend/urls.py
+++ wajo_backend/urls.py
@@ -1,20 +1,3 @@
-"""
-URL configuration for wajo_backend project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.urls import path, include
 
 from django.conf import settings
@@ -22,12 +5,12 @@ from django.conf.urls.static import static
 from core.admin import admin_site
 
 urlpatterns = [
-    path("v1/admin/", admin_site.urls),
-    path("v1/api/auth/", include("accounts.urls")),
-    path("v1/api/onboarding/", include("onboarding.urls")),
-    path("v1/api/notifications/", include("notifications.urls")),
-    path("v1/api/chatbot-admin/", include("chatbot_admin.urls")),
-    path("v1/api/cards/", include("cards.urls")),
+    path("admin/", admin_site.urls),
+    path("api/auth/", include("accounts.urls")),
+    path("api/onboarding/", include("onboarding.urls")),
+    path("api/notifications/", include("notifications.urls")),
+    path("api/chatbot-admin/", include("chatbot_admin.urls")),
+    path("api/cards/", include("cards.urls")),
 ]
 
 
