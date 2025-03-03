4d452b8f99c5b146e6b3a40db461d852d019b543
akashSharmaDev <imakashsharma135@gmail.com>
2025-03-03 11:13:26 +0530

    Updated guest account number and added default image

--- accounts/serializer.py
+++ accounts/serializer.py
@@ -7,6 +7,9 @@ def custom_phone_number_validator(value):
         raise serializers.ValidationError("Invalid phone number format. Please use a valid international phone number format.")
 
 class WajoUserSerializer(serializers.ModelSerializer):
+
+    picture = serializers.SerializerMethodField()
+
     class Meta:
         model = WajoUser
         fields = '__all__'
@@ -18,6 +21,11 @@ class WajoUserSerializer(serializers.ModelSerializer):
         if WajoUser.objects.filter(phone_no=value).exists():
             raise serializers.ValidationError("A user with this phone number already exists.")
         return value
+    
+    def get_picture(self, obj):
+        if not obj.picture or str(obj.picture.url).endswith('null'):
+            return "https://cdn-icons-png.flaticon.com/512/8847/8847419.png"
+        return obj.picture.url
 
 
 class UserRequestSerializer(serializers.ModelSerializer):
