b632497aa850df417cde59bd17c8b9d732a4556b
Mudasar-Makandar <mudassarmmakandar@gmail.com>
2025-07-17 19:38:31 +0530

    improve admin view for calender entry

--- calendar_entry/admin.py
+++ calendar_entry/admin.py
@@ -1,7 +1,14 @@
 from core.admin import admin_site
+from django.contrib import admin
+
 
 from calendar_entry.models import CalendarEventEntry, CalendarGoalEntry
 
+class CalendarEventEntryAdmin(admin.ModelAdmin):
+    list_display = ['user', 'category', 'sub_category', 'date', 'repeat', 'start_time', 'end_time']
 
-admin_site.register(CalendarEventEntry)
-admin_site.register(CalendarGoalEntry)
+class CalendarGoalEntryAdmin(admin.ModelAdmin):
+    list_display = ['user', 'category', 'title',  'start_date', 'end_date']
+
+admin_site.register(CalendarEventEntry, CalendarEventEntryAdmin)
+admin_site.register(CalendarGoalEntry, CalendarGoalEntryAdmin)
