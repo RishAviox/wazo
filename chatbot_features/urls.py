0390fa96b4ff21d5094461d7ae471f1b129a0168
Mudasar-Makandar <mudassarmmakandar@gmail.com>
2025-07-28 16:59:20 +0530

    add CRUD for chatbot events and goals

--- chatbot_features/urls.py
+++ chatbot_features/urls.py
@@ -1,8 +1,26 @@
 from django.urls import path
-from .views import ChatwellnessAPIView, RPEChatAPIView, GameOverviewAPIView
+from .views import *
 
 urlpatterns = [
     path("chat-wellness" , ChatwellnessAPIView.as_view() , name="chat-wellness"),
     path("chat-rpe" , RPEChatAPIView.as_view() , name="chat-rpe"),
-    path("game-overview" , GameOverviewAPIView.as_view() , name="game-overview")
+    # get all the events and goals
+    path("calendar" , CalendarAPIView.as_view() , name="calendar"),
+    
+    # create event
+    path("calendar-event" , CalendarEventAPIViewSet.as_view({'post': 'create'}), name="calendar-event-entry"),
+    # Update and delete event
+    path("calendar-event/<int:pk>" , CalendarEventAPIViewSet.as_view({
+        'patch': 'partial_update',
+        'delete': 'destroy'
+    }), name="calendar-event-entry"),
+    
+    # create goal
+    path("calendar-goal" , CalendarGoalAPIViewSet.as_view({'post': 'create'}), name="calendar-goal-entry"),
+    # Update and delete goal
+    path("calendar-goal/<int:pk>" , CalendarGoalAPIViewSet.as_view({
+        'patch': 'partial_update',
+        'delete': 'destroy'
+    }), name="calendar-goal-entry"),
+    path("game-overview" , GameOverviewAPIView.as_view() , name="game-overview"),
 ]
