e8a83dac8376a1697fd7cdb18b9ff9e170a26ef7
akashSharmaDev <imakashsharma135@gmail.com>
2025-07-29 23:38:46 -0700

    Added models in tracevision app

--- tracevision/models.py
+++ tracevision/models.py
@@ -7,3 +7,20 @@ class MatchDataTracevision(models.Model):
     user = models.ForeignKey(WajoUser, on_delete=models.CASCADE)
     data = models.JSONField(default=dict)
     video = models.FileField(upload_to='match_data_videos/')
+
+class TraceSession(models.Model):
+    match_data = models.ForeignKey(to=MatchDataTracevision, on_delete=models.CASCADE)
+    session_id = models.CharField(max_length=250)
+    match_date = models.DateField()
+    home_team = models.CharField(max_length=100)
+    away_team = models.CharField(max_length=100)
+    home_score = models.PositiveSmallIntegerField()
+    away_score = models.PositiveSmallIntegerField()
+
+
+class TracePlayer(models.Model):
+    name = models.CharField(max_length=100)
+    jersey_number = models.PositiveIntegerField()
+    team = models.CharField(max_length=100)
+    position = models.CharField(max_length=100)
+    session = models.ForeignKey(to=TraceSession, on_delete=models.CASCADE)
