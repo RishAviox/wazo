ae769255d46a8ddbb41ecfb9b3532da5ec33d040
expoweb <expowebdeveloper@gmail.com>
2025-12-19 10:45:21 +0530

    Add GameUserRoleAdmin to manage user roles associated with games in the admin interface.

--- games/admin.py
+++ games/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from core.admin import admin_site
 
-from .models import Game, GameGPSData, GameVideoData, GameMetaData
+from .models import Game, GameGPSData, GameVideoData, GameMetaData, GameUserRole
 from .forms import GameAdminForm, GameGPSDataForm, GameVideoDataForm
 
 
@@ -74,7 +74,42 @@ class GameMetaDataAdmin(admin.ModelAdmin):
     game_display.short_description = "Linked Game"
 
 
+class GameUserRoleAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "user_display",
+        "user_role_display",
+        "game_display",
+        "created_on",
+        "updated_on",
+    )
+    list_filter = ("game", "user__role", "created_on")
+    search_fields = (
+        "user__phone_no",
+        "game__id",
+        "game__name",
+        "user__role",
+    )
+    raw_id_fields = ("game", "user")
+
+    def user_display(self, obj):
+        return obj.user.phone_no if obj.user else "Unknown"
+
+    user_display.short_description = "User"
+
+    def user_role_display(self, obj):
+        return obj.user.role if obj.user and obj.user.role else "No Role"
+
+    user_role_display.short_description = "User Role"
+
+    def game_display(self, obj):
+        return f"{obj.game.id} - {obj.game.name}" if obj.game else "Unknown"
+
+    game_display.short_description = "Game"
+
+
 admin_site.register(Game, GameAdmin)
 admin_site.register(GameGPSData, GameGPSDataAdmin)
 admin_site.register(GameVideoData, GameVideoDataAdmin)
 admin_site.register(GameMetaData, GameMetaDataAdmin)
+admin_site.register(GameUserRole, GameUserRoleAdmin)
